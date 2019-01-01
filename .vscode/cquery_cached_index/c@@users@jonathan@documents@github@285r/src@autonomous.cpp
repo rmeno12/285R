0c7a60826840ee7fc46ca7b3b1ecbaac0cbd2f10
PeriodicallyMe <43706783+PeriodicallyMe@users.noreply.github.com>
2019-01-01 03:24:57 -0600

    i forgot what i did

---
+++ .vscode/cquery_cached_index/c@@users@jonathan@documents@github@285r/src@autonomous.cpp
@@ -0,0 +1,13 @@
+#include "../header files/initAut"
+#include "../header files/waypoints"
+
+void autonomous()
+{
+  profile.generatePath({initRed, redBall}, "Ball");
+  profile.setTarget("Ball", FWD);
+  profile.waitUntilSettled();
+
+  profile.generatePath({initRed, redBall}, "Ball");
+  profile.setTarget("Ball", BWD);
+  profile.waitUntilSettled();
+}
