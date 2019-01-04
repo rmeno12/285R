dc1a37a3c550f6258821e18e55d225e10e39acd9
rmeno12 <menonrahul02@gmail.com>
2019-01-03 20:24:54 -0600

    updated to reflect drivetrain changes

--- src/initOp.cpp
+++ src/initOp.cpp
@@ -20,14 +20,14 @@ Motor l           (7, true,  AbstractMotor::gearset::green);
 
 ChassisControllerIntegrated drive = ChassisControllerFactory::create
 (
-   {-1, -2},
-   {10, 9},
+   {1, -2},
+   {-10, 9},
    AbstractMotor::gearset::green,
    {4_in, 9.78_in}
 );
 
-MotorGroup driveL = MotorGroup({-1, -2});
-MotorGroup driveR = MotorGroup({9, 10});
+MotorGroup driveL = MotorGroup({1, -2});
+MotorGroup driveR = MotorGroup({9, -10});
 
 bool ballIntakeToggle {false};
 bool lazy             {false};
